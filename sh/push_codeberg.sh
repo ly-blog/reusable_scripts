cf9f7154faeaceb6988ed8f1f4eba739f3dd3022
ly <168606077+ly-blog@users.noreply.github.com>
2025-01-12 16:45:05 +0800

    Update push_codeberg.sh

--- sh/push_codeberg.sh
+++ sh/push_codeberg.sh
@@ -19,5 +19,5 @@ git push o main -f
 eval $(ssh-agent)
 (NAME=codeberg HOST=codeberg.org SSHKEY="$SSHKEY_CODEBERG" addKey)
 cat ~/.ssh/config
-ls ~/.ssh
+ls ~/.ssh -lah
 REMOTE="$REMOTE_CODEBERG" HOST=codeberg.org push
