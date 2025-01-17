19cc818a63ca12f0fd6dd4506ec60e3dbf2853aa
ly <100673133+ly-temp@users.noreply.github.com>
2025-01-17 20:20:35 +0800

    Update genIndex.sh

--- sh/genIndex.sh
+++ sh/genIndex.sh
@@ -6,7 +6,7 @@ run(){
 cd blog
 for d in */;do
 	echo "### $d"
-	(cd "$d"&& ls|sed -E "s|(.*).md$|* [\1](<blog/$d\1.md>) \| [html](<blog/$d\1.html>)|g")
+	(cd "$d"&& ls|sed -E "s|(.*).md$|* [\1](<blog/$d\1.md>)  [html](<blog/$d\1.html>)|g")
 	echo ---
 done
 }
